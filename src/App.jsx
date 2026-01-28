b41858561e044f1e46e87fcc569398a7343c9d78
Aarogya <aarogyathapa375@gmail.com>
2026-01-28 18:03:41 +0545

    Update on the code implementation i.e. App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import Chat from "./chat";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
 
 function App() {
-  return <Chat />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
+  );
 }
 
 export default App;
